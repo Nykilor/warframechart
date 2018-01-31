ac3f0c62fd4425da4255c255ee60c946936b2093
Marmolada <smigi1997@gmail.com>
2018-01-31 15:49:47 +0100

    Revert "Initial commit"
    This reverts commit 6374c5401ba779f177101782d80bb78a5539c8d0.

--- resources/cron_worldstate.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_GET['token']) && $_GET['token'] === "worldstatexbps4pc") {
-    require('autoload.php');
-
-    $var = new GetWorldState();
-    $var->getUrlContent();
-    $var->selectedDataToFile("ActiveMissions");
-}
